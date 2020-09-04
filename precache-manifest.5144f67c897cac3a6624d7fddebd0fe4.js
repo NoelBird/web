7e49554d009bb1d1952336a984c76b55561c5f64
NoelBird <lduldu00228@gmail.com>
2020-09-04 22:45:35 +0900

    Updates

---
+++ precache-manifest.5144f67c897cac3a6624d7fddebd0fe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14690f6a2f75501edabf285f50e50915",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "ef02e542b50c68993469",
+    "url": "/web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "65651e993f0bf8279a7a",
+    "url": "/web/static/js/2.70668d50.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/web/static/js/2.70668d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef02e542b50c68993469",
+    "url": "/web/static/js/main.4c2c6d24.chunk.js"
+  },
+  {
+    "revision": "18e908e0716e41df92e2",
+    "url": "/web/static/js/runtime-main.07f5d81b.js"
+  }
+]);
